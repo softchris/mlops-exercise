c02bbd2f99d3c27466da4b60f42d4832592b6ab9
chris <noringc@gmail.com>
2026-07-16 20:06:57 +0100

    Add failing test case for test function

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import os
 import app
 
+def test():
+    assert False
+
 def test_model_file_created():
     app.main()  # Assuming the main function encapsulates the training logic
     assert os.path.exists('models/model.pkl')
